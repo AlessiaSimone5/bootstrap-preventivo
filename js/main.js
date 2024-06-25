b046a25841d170b13f79a93456825b5a9b3d3668
Alessia Simone <alessiasimone597@libero.it>
2024-06-25 15:04:39 +0200

    inserito funzioni principali js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,103 @@
 'use strict';
+
+
+//constanti per operazioni
+const numeroOre = 10;
+const backendOra = 20.50;
+const frontendOra = 15.30;
+const projectOra = 33.60;
+
+
+
+//prezzo
+let backPrezzo = numeroOre * backendOra;
+let frontPrezzo = numeroOre * frontendOra;
+let projectPrezzo = numeroOre * projectOra;
+
+//promo
+const promoUno = 'YHDNU32';
+const promoDue = 'JANJC63';
+const promoTre = 'PWKCN25';
+const promoQuattro = 'SJDPO96';
+const promoCinque = 'POCIE24';
+
+
+
+const bottone = document.getElementById('bottone');
+const codicePromo = document.getElementById('codice-promo');
+const selezione = document.getElementById('selezione');
+
+const prezzoFinale = document.getElementById('prezzo-finale');
+console.log(prezzoFinale);
+
+
+
+
+
+
+//operazioni calcolo
+
+bottone.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (selezione.value === 'backend development') {
+        if (codicePromo.value === promoUno || codicePromo.value === promoDue || codicePromo.value === promoTre || codicePromo.value === promoQuattro || codicePromo.value === promoCinque) {
+            backPrezzo -= (backPrezzo * 25) / 100;
+
+        } else if (codicePromo.value !== '') {
+            alert('Il codice inserito non è valido!');
+        }
+        console.log(backPrezzo);
+        prezzoFinale.innerHTML = `${backPrezzo.toFixed(2)} €`;
+
+
+    } else if (selezione.value === 'frontend development') {
+        if (codicePromo.value === promoUno || codicePromo.value === promoDue || codicePromo.value === promoTre || codicePromo.value === promoQuattro || codicePromo.value === promoCinque) {
+            frontPrezzo -= (frontPrezzo * 25) / 100;
+
+        } else if (codicePromo.value !== '') {
+            alert('Il codice inserito non è valido!');
+        }
+        console.log(frontPrezzo);
+        prezzoFinale.innerHTML = `${frontPrezzo.toFixed(2)} €`;
+
+
+    } else if (selezione.value === 'project analysis') {
+        if (codicePromo.value === promoUno || codicePromo.value === promoDue || codicePromo.value === promoTre || codicePromo.value === promoQuattro || codicePromo.value === promoCinque) {
+            projectPrezzo -= (projectPrezzo * 25) / 100;
+
+        } else if (codicePromo.value !== '') {
+            alert('Il codice inserito non è valido!');
+        }
+        console.log(projectPrezzo);
+        prezzoFinale.innerHTML = `${projectPrezzo.toFixed(2)} €`;
+
+    }
+});
+
+//privacy policy
+const privacyPolicy = document.getElementById('privacy');
+console.log(privacyPolicy);
+
+bottone.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (privacyPolicy.checked) {
+        console.log('');
+    } else
+        alert('Accettare condizioni privacy policy');
+
+
+
+
+});
+
+//campi
+const nome = document.getElementById('nome');
+const cognome = document.getElementById('cognome');
+const email = document.getElementById('email');
+const textarea = document.getElementById('textarea');
+console.log(nome);
+console.log(cognome);
+console.log(email);
+console.log(textarea);
